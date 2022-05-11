d0cba09032a072221fcb5d50c6f46c3221769569
Nelson ANDRADE <52503715+nelsonandrade@users.noreply.github.com>
2022-05-11 19:02:33 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,16 +1,20 @@
+<html>
+    <body>
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 
-<section class="footer">
-    
-        <div class="sociaux">
-            <div class="social"><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></div>
-            <div class="social"><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></div>
-            <div class="social"><a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a></div>
+        <section class="footer">
 
-        </div>
-        <p>&copy;Nelson Andrade 2022</p>
-</section>
+                <div class="sociaux">
+                    <div class="social"><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></div>
+                    <div class="social"><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></div>
+                    <div class="social"><a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a></div>
 
+                </div>
+                <p>&copy;Nelson Andrade 2022</p>
+        </section>
+    </body>
+</html>
   
